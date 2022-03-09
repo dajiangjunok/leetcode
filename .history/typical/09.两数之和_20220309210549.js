4f84ae1aa4f633b8c26cb516f0d018cb8e5609f5
wangyongjie <779503577@qq.com>
2022-03-09 21:29:19 +0800

    两数之和

---
+++ .history/typical/09.两数之和_20220309210549.js
@@ -0,0 +1,21 @@
+// 题：
+// 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+// 你可以按任意顺序返回答案。
+// 例：输入：nums = [3,2,4], target = 6
+// 输出：[1,2]
+ 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+  for(let i=0; i<nums.length; i++){
+      for(let j=i+1;j<nums.length; j++){
+          if(target - nums[i] === nums[j]){
+                return [i,j]
+          }
+      }
+  }
+};
