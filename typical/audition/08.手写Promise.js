4384de0c093970a842683b14eb6df22ad0ccf33e
dajiangjunok <779503577@qq.com>
2022-07-23 23:55:07 +0800

    浏览器垃圾回收

--- typical/audition/08.手写Promise.js
+++ typical/audition/08.手写Promise.js
@@ -79,3 +79,39 @@ const promise = new MyPromise((resolve, reject) => {
 )
 
 console.log('第三步');
+
+
+
+
+
+
+
+// 实现Promise.all 
+Promise.all = function (promises) {
+  return new Promise((resolve, reject) => {
+    // 参数可以不是数组，但必须具有 Iterator 接口
+    if (typeof promises[Symbol.iterator] !== "function") {
+      reject("Type error");
+    }
+    if (promises.length === 0) {
+      resolve([]);
+    } else {
+      const res = [];
+      let count = 0;
+      const len = promises.length;
+      for (let i = 0; i < len; i++) {
+        //考虑到 promises[i] 可能是 thenable 对象也可能是普通值
+        Promise.resolve(promises[i])
+          .then((data) => {
+            res[i] = data;
+            if (++count === len) {
+              resolve(res);
+            }
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      }
+    }
+  });
+};
